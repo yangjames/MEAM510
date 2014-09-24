f366a4005eee05cd5aa920d17227514c985fdac9
James Yang <jayang@seas.upenn.edu>
2014-09-24 19:04:12 -0400

    added header file for m_stuff

--- L3/main.c
+++ L3/main.c
@@ -1,18 +1,9 @@
-/* Name: main.c
- * Author: <insert your name here>
- * Copyright: <insert your copyright message here>
- * License: <insert your license reference here>
- */
+#include <m_general.h>
 
-#include <avr/io.h>
-
-int main(void)
-{
-		DDRE |= 1<<6;
-		PORTE &= !(1<<6);
-    /* insert your hardware initialization here */
-    for(;;){
-        /* insert your main loop code here */
-    }
-    return 0;   /* never reached */
+int main(void) {
+  DDRE |= 1<<6;
+  PORTE &= !(1<<6);
+  for(;;) {
+  }
+  return 0;
 }
