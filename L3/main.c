52418f337ff03e12349aeb2c1aeb29c1ef05fd2f
James Yang <jayang@seas.upenn.edu>
2014-09-24 21:18:54 -0400

    added part 2-1 of L3

--- L3/main.c
+++ L3/main.c
@@ -1,9 +1,14 @@
-#include <m_general.h>
+#include "m_general.h"
+#include <stdint.h>
 
 int main(void) {
   DDRE |= 1<<6;
-  PORTE &= !(1<<6);
-  for(;;) {
+  PORTE |= (1<<6);
+  int i;
+  while(1) {
+    for(i=0;i<30000;++i)
+      asm("nop");
+    PORTE ^=(1<<6);
   }
   return 0;
 }
