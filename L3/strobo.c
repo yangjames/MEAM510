384ff2f1566d66ba2f8031895364de767af0e729
James Yang <james.yang92@gmail.com>
2014-10-03 20:09:25 -0400

    adc works, now working on implementing low and high pass filters

--- L3/strobo.c
+++ L3/strobo.c
@@ -1,6 +1,8 @@
 #include "m_general.h"
 #include <stdint.h>
 
+#define PI 3.14159265359
+
 volatile uint64_t tim1_ovf;
 
 ISR(TIMER1_OVF_vect) {
@@ -8,8 +10,11 @@ ISR(TIMER1_OVF_vect) {
 }
 
 int main(void) {
+  /* set debugging LED output registers */
+  DDRB = 0xFF;
+
   /* set initial overflow value to 0 */
-  tim0_ovf = 0;
+  tim1_ovf = 0;
 
   /* clear global interrupts */
   cli();
@@ -20,17 +25,16 @@ int main(void) {
 
   /* configure ADC */
   ADMUX |= (1 << REFS0); // set reference voltage to VCC 5V
-  ADCSRA |= (1 << ADEN); // enable ADC
+  ADCSRA |= (1 << ADEN) | (1 << ADPS1) | (1 << ADPS0); // enable ADC, divide by 8
 
   /* configure timer 1 */
   TCCR1A = 0x00; // normal compare output mode operation
   TCNT1 = 0x00; // intial timer value = 0
+  TIMSK1 = 0x01; // enable overflow interrupt
+  TCCR1B = 0x01; // enable timer
 
   /* enable global interrupts */
   sei();
-
-  /* enable timer 1 */
-  TCCR1B = 0x03; // no timer prescale
 
   /* declare variables */
   float current_time_stamp = 0;
@@ -38,12 +42,17 @@ int main(void) {
 
   float left_channel = 0.0;
   float right_channel = 0.0;
+  float avg_channel = 0.0;
 
-  int high_pass_pwm = 0;
-  int low_pass_pwm = 0;
+  float cutoff_low = 200; // low pass filter cutoff frequency in Hz
+  float RC_low = 1/(cutoff_low*2*PI);
+  float alpha_low = 0.0;
+  float low_pass_out = 0.0;
 
-  /* set debugging LED output registers */
-  DDRB = 0x03;
+  float cutoff_high = 2000; // high pass filter cutoff frequency in Hz
+  float RC_high = 1/(cutoff_high*2*PI);
+  float alpha_high = 0.0;
+  float high_pass_out = 0.0;
 
   /* main loop */
   while(1) {
@@ -52,22 +61,28 @@ int main(void) {
     ADMUX &= ~(1 << MUX0);
     ADCSRA |= (1 << ADSC);
     while (ADCSRA & (1 << ADSC));
-    left_channel = (float)(ADC)/1024;
-
+    left_channel = (float)(ADCW)/1024;
     ADMUX |= (1 << MUX0);
     ADCSRA |= (1 << ADSC);
     while (ADCSRA & (1 << ADSC));
-    right_channel = (float)(ADC)/1024;
+    right_channel = (float)(ADCW)/1024;
 
-    if (left_channel > 0.5)
-      PORTB |= (1 << 0);
-    else
-      PORTB &= ~(1 << 0);
+    /* average left and right channels */
+    avg_channel = (left_channel + right_channel)/2;
+    
+    /* get time stamp */
+    current_time_stamp = (float)(TCNT1 + tim1_ovf*65536)/16000000;
+    tim1_ovf = 0;
+    TCNT1 = 0;
 
-    if (right_channel > 0.5)
-      PORTB |= (1 << 0);
-    else
-      PORTB &= ~(1 << 0);
+    /* low pass filter */
+    low_pass_out = 1/(RC_high/current_time_stamp + 1)*avg_channel;
+
+    /* high pass filter */
+    high_pass_out = RC_high/current_time_stamp/(RC_high/current_time_stamp + 1);
+
+    /* calculate pwm */
+    
   }
 
   return 0;
