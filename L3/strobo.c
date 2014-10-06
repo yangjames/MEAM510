8e581b77f4763f9a888672c434a4207ddb69481b
James Yang <james.yang92@gmail.com>
2014-10-05 20:16:45 -0400

    everything works

--- L3/strobo.c
+++ L3/strobo.c
@@ -5,6 +5,8 @@
 #define PI 3.14159265359
 
 volatile uint64_t tim1_ovf;
+volatile int test, D_1, D_2, D_3, D_4;
+float SF;
 
 ISR(TIMER1_OVF_vect) {
   tim1_ovf++;
@@ -26,9 +28,47 @@ ISR(TIMER3_OVF_vect) {
   PORTB &= ~(1 << 6);
 }
 
+void reset_timers() {
+  TIMSK0 = 0x00;
+  TIMSK3 = 0x00;
+}
+
+void enable_timers() {
+  TIMSK0 = 0x03;
+  TIMSK3 = 0x03;
+}
+
+void audio_timer1() {
+  TCCR1A = 0x00; // normal compare output mode operation
+  TCNT1 = 0x00; // intial timer value = 0
+  TIMSK1 = 0x01; // enable overflow interrupt
+  TCCR1B = 0x01; // enable timer
+}
+
+void strobo_timer1() {
+  // Set Timer 1 Prescaler /64 [31.25 kHz]
+  /* clear(TCCR1B,CS12); */
+  /* set(TCCR1B,CS11); */
+  /* set(TCCR1B,CS10); */
+    
+  /* // Set Timer Mode - Mode 15 - */
+  /* set(TCCR1B,WGM13); */
+  /* set(TCCR1B,WGM12); */
+  /* set(TCCR1A,WGM11); */
+  /* set(TCCR1A,WGM10); */
+
+  /* set(TCCR1A, COM1B1); */
+  /* clear(TCCR1A, COM1B0); */
+  TIMSK1 = 0x00;
+  TCCR1A = 0x23;
+  TCNT1 = 0x00;
+  TCCR1B = 0x1B;
+}
+
 int main(void) {
   /* set debugging LED output registers */
   DDRB = 0xFF;
+  DDRD = 0x00;
 
   /* set initial overflow value to 0 */
   tim1_ovf = 0;
@@ -85,41 +125,79 @@ int main(void) {
   float high_pass_out = 0.0;
   float alpha_high = 0.0;
 
+  char operation_mode = 0;
   /* main loop */
   while(1) {
+    if(PIND & (1 << 6)) {
+      if (!operation_mode) {
+	cli();
+	/* set clock to full 16MHz */
+	CLKPR = 1 << CLKPCE;
+	CLKPR = 0x00;
+	enable_timers();
+	audio_timer1();
+	ADMUX &= ~(1 << MUX2);
+	operation_mode = 1;
+	sei();
+      }
 
-    /* read adc values from left and right audio channels*/
-    ADMUX &= ~(1 << MUX0);
-    ADCSRA |= (1 << ADSC);
-    while (ADCSRA & (1 << ADSC));
-    left_channel = (float)(ADCW)/1024;
-    ADMUX |= (1 << MUX0);
-    ADCSRA |= (1 << ADSC);
-    while (ADCSRA & (1 << ADSC));
-    right_channel = (float)(ADCW)/1024;
+      /* read adc values from left and right audio channels*/
+      ADMUX &= ~(1 << MUX0);
+      ADCSRA |= (1 << ADSC);
+      while (ADCSRA & (1 << ADSC));
+      left_channel = (float)(ADCW)/1024;
+      ADMUX |= (1 << MUX0);
+      ADCSRA |= (1 << ADSC);
+      while (ADCSRA & (1 << ADSC));
+      right_channel = (float)(ADCW)/1024;
 
-    /* average left and right channels */
-    avg_channel = (left_channel + right_channel)/2 - 0.5; // 0 < avg_channel < 1
+      /* average left and right channels */
+      avg_channel = (left_channel + right_channel)/2 - 0.5; // 0 < avg_channel < 1
     
-    /* get time stamp */
-    current_time_stamp = (float)(TCNT1 + tim1_ovf*65536)/16000000;
-    tim1_ovf = 0;
-    TCNT1 = 0;
+      /* get time stamp */
+      current_time_stamp = (float)(TCNT1 + tim1_ovf*65536)/16000000;
+      tim1_ovf = 0;
+      TCNT1 = 0;
 
-    /* low pass filter */
-    alpha_low = current_time_stamp/(RC_low+current_time_stamp);
-    low_pass_out = alpha_low*avg_channel + (1-alpha_low)*low_pass_out;
+      /* low pass filter */
+      alpha_low = current_time_stamp/(RC_low+current_time_stamp);
+      low_pass_out = alpha_low*avg_channel + (1-alpha_low)*low_pass_out;
 
-    /* high pass filter */
-    alpha_high = RC_high/(current_time_stamp + RC_high);
-    high_pass_out = alpha_high*high_pass_out + alpha_high*(avg_channel - avg_channel_previous);
-    avg_channel_previous = avg_channel;
+      /* high pass filter */
+      alpha_high = RC_high/(current_time_stamp + RC_high);
+      high_pass_out = alpha_high*high_pass_out + alpha_high*(avg_channel - avg_channel_previous);
+      avg_channel_previous = avg_channel;
 
-    /* calculate pwm duty cycle */
-    float duty_cycle_low = low_pass_out>=0.05 ? low_pass_out : 0;
-    float duty_cycle_high = high_pass_out >= 0.05 ? high_pass_out : 0;
-    OCR0A = (int)(duty_cycle_low*256);
-    OCR3A = (int)(duty_cycle_high*65536);
+      /* calculate pwm duty cycle */
+      float duty_cycle_low = low_pass_out>=0.05 ? low_pass_out : 0;
+      float duty_cycle_high = high_pass_out >= 0.05 ? high_pass_out : 0;
+      OCR0A = (int)(duty_cycle_low*256);
+      OCR3A = (int)(duty_cycle_high*65536);
+    }
+    else {
+      if (operation_mode) {
+	cli();
+	m_clockdivide(3);
+	ADMUX &= ~(1 << MUX0);
+	ADMUX |= (1 << MUX2);
+	reset_timers();
+	strobo_timer1();
+	operation_mode = 0;
+	sei();
+      }
+      ADCSRA |= (1 << ADSC);
+      while(ADCSRA & (1 << ADSC));
+      test = ADC;
+      D_1 = 8*check(PIND,4);
+      D_2 = 4*check(PIND,5);
+      D_3 = 2*check(PIND,2);
+      D_4 = 1*check(PIND,3);
+
+      SF = (1 + D_1 + D_2 + D_3 + D_4)/17.0;
+
+      OCR1A = 50*test;
+      OCR1B = (int)(SF*OCR1A);
+    }
   }
 
   return 0;
