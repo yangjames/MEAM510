4c4ef8f7340a1c1b29506f25ed3b4247cdde1c6a
James Yang <james.yang92@gmail.com>
2014-10-23 01:19:58 -0400

    adc works, need to setup timer next

--- L5/main.c
+++ L5/main.c
@@ -1,19 +1,47 @@
-#include m_general.h
-#include m_usb.c
+#include "m_general.h"
+#include "m_usb.c"
+
+typedef enum {
+  SEARCH,
+  STOP,
+  SHOOT
+};
+
+void init();
 
 int main(void) {
-  //init();
-  
+  init();
+
+  PORTE &= ~(1 << 6);
+  ADCSRA |= (1 << ADSC);
+
+  while(1) {
+  }
+
   return 0;
 }
 
 void init() {
+  cli();
+  m_usb_init();
+  while(!m_usb_isconnected());
+
   /* initialize kicker pin */
   DDRB |= (1 << 6);
   PORTB |= (1 << 6);
   
-  /* configure ADC */
-  ADMUX |= (1 << REFS0); // set reference voltage to VCC 5V
-  ADCSRA |= (1 << ADEN); // enable ADC
+  DDRE |= (1 << 6);
+  PORTE |= (1 << 6);
 
+  /* configure ADC */
+  ADMUX |= (1 << REFS0) | (1 << MUX0); // set reference voltage to VCC 5V
+  ADCSRA |= (1 << ADEN) | (1 << ADIE); // enable ADC
+  
+  sei();
+}
+
+ISR(ADC_vect) {
+  m_usb_tx_int(ADCW);
+  m_usb_tx_string("\n\r");
+  ADCSRA |= (1 << ADSC);
 }
