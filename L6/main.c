7823b4bcef1e2a13d5701019119321a89756f882
James Yang <james.yang92@gmail.com>
2014-11-07 12:02:01 -0500

    got angle measurements

--- L6/main.c
+++ L6/main.c
@@ -6,6 +6,7 @@
 #include "m_usb.h"
 #include "m_usb.c"
 #include <stdint.h>
+#include <math.h>
 
 #define PI 3.14159265359
 #define SCALE (9.81/16384)
@@ -15,13 +16,17 @@ volatile uint64_t tim3_ovf;
 void init();
 
 int main(void) {
+  init();
+
   /* initialize physical parameters */
-  float x_acc = 0.0, z_acc = 0.0, pitch = 0.0;
+  float x_acc = 0.0, z_acc = 0.0, pitch = 0.0, pitch_gyro = 0.0;
   float alpha_lp = 0.0, alpha_hp = 0.0;
+  float alpha = 0.98;
   float cutoff_low = 30.0, cutoff_high = 30.0;
   float RC_low = 1/(cutoff_low*2*PI);
   float RC_high = 1/(cutoff_high*2*PI);
   float Kp = 0.0, Kd = 0.0, Ki = 0.0;
+  float dt = 0.0;
 
   /* initialize IMU storage buffer */
   int data[9];
@@ -48,8 +53,11 @@ int main(void) {
       /* recalculate x and z accelerations and pitch angle */
       x_acc = x_acc*alpha_lp+(1-alpha_lp)*SCALE*data[0];
       z_acc = z_acc*alpha_lp+(1-alpha_lp)*SCALE*data[2];
-      pitch = pitch*alpha_hp+(1-alpha_hp)*dt*data[4];
+      pitch_gyro = pitch_gyro*alpha_hp+(1-alpha_hp)*dt*data[4];
+      pitch = atan2f(x_acc,z_acc)*alpha + (1-alpha)*pitch_gyro;
 
+      m_usb_tx_int((int)(pitch*180/PI));
+      m_usb_tx_string("\n\r");
       /* m_usb_tx_int(data[0]); */
       /* m_usb_tx_string("\t"); */
       /* m_usb_tx_int(data[1]); */
@@ -89,4 +97,8 @@ void init() {
   TCCR3B = 0x01; // enable timer
 
   sei();
+}
+
+ISR(TIMER3_OVF_vect) {
+  tim3_ovf++;
 }
