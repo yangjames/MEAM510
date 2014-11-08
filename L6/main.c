89972620e17297d9e555e942e58a688837c438d8
James Yang <james.yang92@gmail.com>
2014-11-08 16:50:15 -0500

    gains are tuned

--- L6/main.c
+++ L6/main.c
@@ -1,6 +1,9 @@
+#include <avr/eeprom.h>
 #include "m_general.h"
 #include "m_bus.h"
 #include "m_bus.c"
+#include "m_rf.h"
+#include "m_rf.c"
 #include "m_imu.h"
 #include "m_imu.c"
 #include "m_usb.h"
@@ -9,29 +12,60 @@
 #include <math.h>
 
 #define PI 3.14159265359
-#define ACC_SCALE (9.81/16384)
+#define g 9.81
+#define ACC_SCALE (g/16384)
 #define GYRO_SCALE (125*PI/180/16384)
+#define DUTY_LIMIT 0.9
+
+#define packet_length 8
+#define channel 2
+#define address 0x10
+
+typedef enum {
+  KP_POS = 46,
+  KD_POS = KP_POS+sizeof(float),
+  KI_POS = KD_POS+sizeof(float),
+  INT_CAP_POS = KI_POS+sizeof(float)
+};
 
 volatile uint64_t tim3_ovf;
+volatile int read_flag;
 
 void init();
 
 int main(void) {
   init();
+  
+  /* radio module initialize */
+  char packet[packet_length] = {0,0,0,0,0,0,0,0};
+  char* buffer=&packet[0];
+  char success;
+
+  /* set gains */
+  eeprom_update_float((float*)KP_POS, 4.2);
+  eeprom_update_float((float*)KD_POS, 0.23);
+  eeprom_update_float((float*)KI_POS, 0.17);
+  eeprom_update_float((float*)INT_CAP_POS, 50.0);
 
   /* initialize physical parameters */
   float x_acc = 0.0, z_acc = 0.0, pitch = 0.0, prev_err = 0.0, pitch_gyro = 0.0, d_err = 0.0;
   float alpha_lp = 0.0, alpha_hp = 0.0;
   float alpha = 0.98;
-  float cutoff_low = 30.0, cutoff_high = 30.0;
+  float cutoff_low = 30.0, cutoff_high = 0.1;
   float RC_low = 1/(cutoff_low*2*PI);
   float RC_high = 1/(cutoff_high*2*PI);
-  float Kp = 1.0, Kd = 0.0, Ki = 0.0;
   float dt = 0.0;
-  float torque = 0.0, duty_cycle = 0.0, sat_duty_cycle = 0.0;
-  float pitch_d = 0.0;
+  float duty_cycle = 0.0;
+  float pitch_d = -PI/180;
   float err = 0.0, err_int = 0.0;
+  float command = 0.0;
 
+  /* obtain controller gains */
+  float Kp = eeprom_read_float((float*)KP_POS),
+    Kd = eeprom_read_float((float*)KD_POS),
+    Ki = eeprom_read_float((float*)KI_POS),
+    int_cap = eeprom_read_float((float*)INT_CAP_POS);
+  
   /* initialize IMU storage buffer */
   int data[9];
 
@@ -44,8 +78,43 @@ int main(void) {
   else
     PORTE &= ~(1 << 6);
 
+  /* while(1) { */
+  /*   m_usb_tx_int((int)(Kp*10)); */
+  /*   m_usb_tx_string(" "); */
+  /*   m_usb_tx_int((int)(Kd*10)); */
+  /*   m_usb_tx_string(" "); */
+  /*   m_usb_tx_int((int)(Ki*10)); */
+  /*   m_usb_tx_string(" "); */
+  /*   m_usb_tx_int((int)(int_cap*10)); */
+  /*   m_usb_tx_string("\n\r"); */
+  /* } */
+
   /* main loop */
   while(1) {
+    if (read_flag) {
+      success = m_rf_read(buffer, packet_length);
+
+      if (success) {
+	m_green(ON);
+	m_red(OFF);
+      }
+      else {
+	m_red(OFF);
+	m_red(ON);
+      }
+      Kp = (float)(256*packet[0]+packet[1])/1000.0;
+      Kd = (float)(256*packet[2]+packet[3])/1000.0;
+      Ki = (float)(256*packet[4]+packet[5])/1000.0;
+      int_cap = (float)(256*packet[6]+packet[7])/1000.0;
+
+      eeprom_update_float((float*)KP_POS, Kp);
+      eeprom_update_float((float*)KD_POS, Kd);
+      eeprom_update_float((float*)KI_POS, Ki);
+      eeprom_update_float((float*)INT_CAP_POS, int_cap);
+      
+      read_flag = 0;
+    }
+
     if (m_imu_raw(data)) {
       /* get delta t */
       dt = (float)(TCNT3 + tim3_ovf*65536)/16000000;
@@ -59,17 +128,25 @@ int main(void) {
       /* recalculate x and z accelerations and pitch angle */
       x_acc = x_acc*alpha_lp+(1-alpha_lp)*ACC_SCALE*data[1];
       z_acc = z_acc*alpha_lp+(1-alpha_lp)*ACC_SCALE*data[2];
-      pitch_gyro = pitch_gyro*alpha_hp+(1-alpha_hp)*dt*GYRO_SCALE*data[3];
-      pitch = atan2f(x_acc,z_acc)*alpha + (1-alpha)*pitch_gyro;
+      pitch += dt*GYRO_SCALE*data[3];
+      if (abs(x_acc)+abs(z_acc)> -2*g && abs(x_acc)+abs(z_acc)< 2*g)
+	pitch = pitch*alpha + atan2f(x_acc,z_acc)*(1-alpha);
 
       /* calculate error, error derivative, and integral error */
       err = pitch_d - pitch;
       d_err = (err - prev_err)/dt;
       prev_err = err;
-      err_int = (fabs(err_int + err) >= 10) ? 10*(err_int > 0) : err_int+err;
+      err_int = (fabs(err_int + err) >= int_cap) ? int_cap*(2*(err_int+err > 0)-1) : err_int+err;
+
+      /* set duty cycle */
+      command = Kp*err + Kd*d_err + Ki*err_int;
+      if (fabs(command) > DUTY_LIMIT)
+	duty_cycle = DUTY_LIMIT;
+      else
+	duty_cycle = fabs(command);
 
       /* set motor direction */
-      if (err > 0) {
+      if (command > 0) {
 	PORTB |= (1 << 5);
 	PORTC &= ~(1 << 6);
       }
@@ -77,27 +154,15 @@ int main(void) {
 	PORTB &= ~(1 << 5);
 	PORTC |= (1 << 6);
       }
-
-      /* set duty cycle */
-      duty_cycle = fabs(Kp*err + Kd*d_err + Ki*err_int);
-      if (duty_cycle > 0.8)
-	duty_cycle = 0.8;
-      OCR1B = fabs(duty_cycle)*OCR1A;
-
-      /* m_usb_tx_int(OCR1B); */
-      /* m_usb_tx_string("\n\r"); */
-      /* m_usb_tx_int(data[0]); */
-      /* m_usb_tx_string("\t"); */
-      /* m_usb_tx_int(data[1]); */
-      /* m_usb_tx_string("\t"); */
-      /* m_usb_tx_int(data[2]); */
-      /* m_usb_tx_string("\t"); */
-      /* m_usb_tx_int(data[3]); */
-      /* m_usb_tx_string("\t"); */
-      /* m_usb_tx_int(data[4]); */
-      /* m_usb_tx_string("\t"); */
-      /* m_usb_tx_int(data[5]); */
-      /* m_usb_tx_string("\n\r"); */
+      OCR1B = duty_cycle*OCR1A;
+      
+      if (fabs(pitch) > 80*PI/180) {
+	OCR1B = 0;
+	while(1);
+      }
+      //m_usb_tx_int((int)(err_int));
+      //m_usb_tx_int((int)(GYRO_SCALE*data[3]));
+      //m_usb_tx_string("\n\r");
     }
   }
   return 0;
@@ -116,6 +181,7 @@ void init() {
 
   /* initialize i2c port and usb port */
   m_bus_init();
+  while(!m_rf_open(channel, address, packet_length));
   m_usb_init();
 
   /* configure motor direction pins */
@@ -143,4 +209,8 @@ void init() {
 
 ISR(TIMER3_OVF_vect) {
   tim3_ovf++;
+}
+
+ISR(INT2_vect) {
+  read_flag = 1;
 }
